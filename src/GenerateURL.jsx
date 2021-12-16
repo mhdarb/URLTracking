53613bf5cc2f633905e39a8b30de69c123fe55f3
maj14 <91468991+maj14@users.noreply.github.com>
2021-12-16 19:30:52 +0530

    Hashed url using crypto js

--- src/GenerateURL.jsx
+++ src/GenerateURL.jsx
@@ -3,30 +3,43 @@ import { useState } from 'react'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { SendOutlined } from '@mui/icons-material';
+import cryptoJs from 'crypto-js';
+var CryptoJS = require("crypto-js");
+
 
 export default function GenerateURL() {
     const [copied, setCopied] = useState(false);
+    let url=''
     const handleSubmit = (e) =>{
         e.preventDefault();
-        let url=''
         if(websiteUrl.length>0){
             url=`https://${websiteUrl}/?`;
         }
-        if(source.length>0){
-            url+="utm_source="+source;
-        } 
-        if(medium.length>0){
-            url+="&"+"utm_medium="+medium;
-        } 
-        if(campaign.length>0){
-            setCampaign(campaign.split(' ').join('-'))
-            url+="&"+"utm_campaign="+campaign;
-        } 
-        if(term.length>0){
-            setTerm(term.split(' ').join('-'))
-            url+="&"+"utm_term="+term;
-        } 
+        addQueryParameter("utm_source=",source,false);
+        addQueryParameter("utm_medium=",medium,true);
+        addQueryParameter("utm_campaign=",campaign,true);
+        addQueryParameter("utm_term=",term,true);
+        
         setHashedUrl(url);
+    }
+
+    const addQueryParameter = (queryName,query,addAnd) =>{
+        if(query.length>0){
+            console.log("yes")
+            url+=(addAnd?"&":"")+queryName+query;
+            console.log(url)
+        }
+    }
+
+    const encryptQuery = (queryParam) =>{
+        const hashedQueryParam = CryptoJS.AES.encrypt(queryParam,"secretKey").toString();
+        return hashedQueryParam
+    }
+
+    const decryptQuery = (hashedQuery) =>{
+        let originalQuery = cryptoJs.AES.decrypt(hashedQuery,"secretKey")
+        originalQuery = originalQuery.toString(CryptoJS.enc.Utf8)
+        return originalQuery
     }
 
     const [websiteUrl, setWebsiteUrl] = useState("")
@@ -57,23 +70,41 @@ export default function GenerateURL() {
             <form action="" onSubmit={handleSubmit}>
                 <div className="formInput">
                     <label>WEBSITE URL </label>
-                    <input type="text" required="true" placeholder='ex. google.com' onChange={(e)=>setWebsiteUrl(e.target.value)}/>
+                    <input type="text" required placeholder='ex. google.com' onChange={(e)=>setWebsiteUrl(e.target.value)}/>
                 </div>
                 <div className="formInput">
                     <label>UTM_SOURCE</label>
-                    <input type="text" required="true" placeholder='ex. facebook, google, newsletter' onChange={(e)=>setSource(e.target.value)}/>
+                    <input type="text" required placeholder='ex. facebook, google, newsletter' 
+                        onChange={(e)=>{
+                            const hashedQueryParam = encryptQuery(e.target.value);
+                            setSource(hashedQueryParam)
+                        }}
+                    />
                 </div>
                 <div className="formInput">
                     <label>UTM_MEDIUM</label>
-                    <input type="text" required="true" placeholder='ex: cpc, display, email, social' onChange={(e)=>setMedium(e.target.value)}/>
+                    <input type="text" required placeholder='ex: cpc, display, email, social'  
+                        onChange={(e)=>{
+                            const hashedQueryParam = encryptQuery(e.target.value);
+                            setMedium(hashedQueryParam)
+                        }}
+                    />
                 </div>
                 <div className="formInput">
                     <label>UTM_CAMPAIGN</label>
-                    <input type="text" placeholder='ex: "spring_shoes", "50%off_sale"' onChange={(e)=>setCampaign(e.target.value)}/>
+                    <input type="text" placeholder='ex: "spring_shoes", "50%off_sale"'  
+                        onChange={(e)=>{
+                            const hashedQueryParam = encryptQuery(e.target.value);
+                            setCampaign(hashedQueryParam)
+                        }}/>
                 </div>
                 <div className="formInput">
                     <label>UTM_TERM</label>
-                    <input type="text" placeholder='ex: "best marketing software"' onChange={(e)=>setTerm(e.target.value)}/>
+                    <input type="text" placeholder='ex: "best marketing software"' 
+                        onChange={(e)=>{
+                            const hashedQueryParam = encryptQuery(e.target.value);
+                            setTerm(hashedQueryParam)
+                        }}/>
                 </div>
                 <button >GENERATE URL <SendOutlined style={{marginLeft:"5px"}}/></button>
             </form>           
